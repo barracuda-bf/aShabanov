038a75ec130902b22de92bd15e2f2ae19948b3f8
aShabanov <barracudablackfire@mail.ru>
2017-03-29 14:21:05 +0600

    внесение изменений

--- src/com/company/lab2/Task37.java
+++ src/com/company/lab2/Task37.java
@@ -1,11 +1,31 @@
 package com.company.lab2;
 
+import java.util.Scanner;
+
 /**
  * Created by наш on 29.03.2017.
  */
 public class Task37 {
     public static void main(String[] args) {
+        int a;  //v1
+        int b;  // t1
+        int c; // v2
+        int d; // t2
+        Scanner skan = new Scanner(System.in);
+        System.out.println("Selekt v1:");
+        a = skan.nextInt();
+        System.out.println("Selekt t1:");
+        b = skan.nextInt();
+        System.out.println("Selekt v2:");
+        c = skan.nextInt();
+        System.out.println("Selekt t2:");
+        d = skan.nextInt();
+        int v1 = a+c;
+        double t = ((b*a)+(d*c))/(a+c);
+        System.out.println("V= "+v1);
 
+        System.out.print("T= ");
+        System.out.format("%.4f%n",t);
             }
 
 }
